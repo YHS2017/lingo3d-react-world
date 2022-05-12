00d72a2a8b8775ccf584a3659c7b232bc5737d00
YHS2017 <594726016@qq.com>
2022-05-12 10:31:52 +0800

    前后端整合

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const io = require('socket.io');
-const Game = require('./src/game');
+const Game = require('./game');
 
 const app = express();
 
